23fb2b131d0cb4e477d756ecb1d567fdebde6915
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2019-11-20 13:50:11 -0300

    added create html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,32 @@ class UsersController < ApplicationController
   def index
   end
 
-  def show
+  def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    if @user.save
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+     @user = User.find(params[:id])
+   end
 
   def new
     @user = User.new
   end
 
   def create
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    @user = User.new(user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    #@user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
